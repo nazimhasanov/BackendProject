daf99de383b97bd9288e7d003c9cd6727593e694
nazimhasanov <nazimnh@academy.edu.az>
2021-06-07 18:43:19 +0400

    Add project files.

---
+++ BackendProject/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using BackendProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
