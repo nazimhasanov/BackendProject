e81f622d4f62159edb544a12955f5ad2fd2a3192
nazimhasanov <nazimnh@academy.edu.az>
2021-06-07 20:37:49 +0400

    HomeViewModel is done

---
+++ BackendProject/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BackendProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendProject.ViewModel
+{
+    public class HomeViewModel
+    {
+        public List<Slider> Slider { get; set; }
+    }
+}
